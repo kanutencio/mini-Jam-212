08d6320a1d08619849c71c86414226f9a5dc6792
kanutencio <pradillacastroalejandro@gmail.com>
2026-06-05 19:45:02 -0500

    correciones

--- Assets/Scrips/Scripts kanutencio/TopButtonPanel.cs
+++ Assets/Scrips/Scripts kanutencio/TopButtonPanel.cs
@@ -4,14 +4,12 @@ using UnityEngine.UI;
 
 public class TopButtonPanel : MonoBehaviour
 {
-    [SerializeField] private Transform buttonContainer;
-    [SerializeField] private GameObject buttonPrefab;
 
     private List<GameObject> buttons = new List<GameObject>();
 
     public void AddButton(GameObject button, Transform parent)
     {
-        GameObject btn = Instantiate(buttonPrefab, buttonContainer);
+        GameObject btn = Instantiate(button, parent);
         buttons.Add(btn);
     }
 }
