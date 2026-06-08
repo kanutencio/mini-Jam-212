5efbd0c162244edce773d4a1deba2f7d16899db2
kanutencio <pradillacastroalejandro@gmail.com>
2026-06-07 19:43:06 -0500

    avances de la pantalla de titulo

--- Assets/Scrips/ScriptMenu.cs
+++ Assets/Scrips/ScriptMenu.cs
@@ -1,8 +1,18 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScriptMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Play")]
+    [SerializeField] private float velocidad;
+    [SerializeField] private GameObject Punto0;
+    [SerializeField] private GameObject Punto1;
+    [SerializeField] private GameObject Punto2;
+    [SerializeField] private int puntoActual;
+    private Vector3 destino;
+
     void Start()
     {
         
@@ -11,6 +21,40 @@ public class ScriptMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(
+        transform.position,
+        destino,
+        velocidad * Time.deltaTime
+        );
+    }
+
+    public void Jugar()
+    {
+        velocidad = 7;
+        destino = Punto1.transform.position;
+        StartCoroutine("CambioScena");
+    }
+
+    IEnumerator CambioScena()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Pruebas Kanutencio");
+    }
+
+    public void Instruciones()
+    {
+        velocidad = 12;
+        destino = Punto2.transform.position;
+    }
+
+    public void SalirDelJuego()
+    {
+        // Cierra la aplicación (Funciona en compilaciones de PC/Android)
+        Application.Quit();
+
+        // Si estás probando en el Editor de Unity, esto detiene el modo juego
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
